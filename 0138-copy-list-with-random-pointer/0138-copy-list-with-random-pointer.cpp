acb8c1f3cd0809d03cbe56ee3b9b12b9ae579a68
Divyam Kumar Choubey <divyam1605@gmail.com>
2026-07-27 21:11:52 +0530

    Time: 12 ms (14.87%), Space: 15.2 MB (27.79%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -16,15 +16,6 @@ public:
 
 class Solution {
 public:
-    Node* find(Node* cur1, Node* cur2, Node* x){
-        if(x==NULL)
-            return NULL;
-        while(cur1 != x){
-            cur1 = cur1->next;
-            cur2 = cur2->next;
-        }
-        return cur2;
-    }
     Node* copyRandomList(Node* head) {
         Node* newHead = new Node(0);
         Node* tail = newHead;
@@ -39,12 +30,22 @@ public:
         delete tail;
         tail = newHead;
         temp = head;
+       
+        unordered_map<Node*, Node*> m;
         while(temp){
-            tail->random = find(head, newHead, temp->random);
+            m[temp] = tail;
+            temp = temp->next;
+            tail = tail->next;
+        }
+        temp = head;
+        tail = newHead;
+
+        while(temp){
+            tail->random = m[temp->random];
             tail = tail->next;
             temp = temp->next;
         }
-
         return newHead;
+
     }
 };
