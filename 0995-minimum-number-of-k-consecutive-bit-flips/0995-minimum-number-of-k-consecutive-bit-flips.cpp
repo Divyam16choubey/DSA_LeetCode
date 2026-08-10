5dcb7537118a468a899e80084736c16a1f1b614c
Divyam Kumar Choubey <divyam1605@gmail.com>
2026-08-10 11:39:04 +0530

    Time: 13 ms (11.21%), Space: 112.1 MB (62.96%) - LeetHub

--- 0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.cpp
+++ 0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.cpp
@@ -7,6 +7,7 @@ public:
         for(int i=0; i<nums.size(); i++){
             if(!q.empty() && q.front()<i)
                 q.pop();
+            //we have to flip only when there is 0, so when there is 1 and no. of flip(q.size() is odd we have to flip and for the case of 0 and no. of flips is even then we have to flip otherwise no flip required).
             if(q.size()%2 == nums[i]){
                 if(i+k-1>=nums.size())
                     return -1;
