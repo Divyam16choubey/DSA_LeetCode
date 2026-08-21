646be7efd0c3c56c9c6324ef3b84a20bcfa9379b
Divyam Kumar Choubey <divyam1605@gmail.com>
2026-08-21 23:10:12 +0530

    Time: 0 ms (100%), Space: 21.9 MB (38.91%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+
+        long long totalSum = 1LL * n * (n+1) / 2;
+        long long sum = 0;
+
+        for(int i=0; i<n; i++){
+            sum += nums[i];
+        }
+
+        return (totalSum-sum);
+    }
+};
